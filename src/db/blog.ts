00276e2f5336f5cde9ee9a61dcb160fabd4e185c
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2025-05-16 21:14:51 +0700

    Update blog.ts

--- src/db/blog.ts
+++ src/db/blog.ts
@@ -6,11 +6,40 @@ const PostSchema = new mongoose.Schema(
     content: { type: String, required: true },
     image: { type: String },
     author: { type: String, required: true },
+    kategori: {
+      type: String,
+      enum: ['Shitpost USK', 'Informasi USK', 'Hiburan', 'Lain-lain'],
+      default: 'Shitpost USK'
+    },
+    fakultas: {
+      type: String,
+      enum: [
+        'Umum', // Untuk Sekolah Pascasarjana dan postingan umum
+        'Ekonomi dan Bisnis (FEB)',
+        'Hukum (FH)',
+        'Keguruan dan Ilmu Pendidikan (FKIP)',
+        'Teknik (FT)',
+        'Kedokteran (FK)',
+        'Pertanian (FP)',
+        'Matematika dan Ilmu Pengetahuan Alam (FMIPA)',
+        'Kedokteran Hewan (FKH)',
+        'Kelautan dan Perikanan (FKP)',
+        'Ilmu Sosial dan Ilmu Politik (FISIP)',
+        'Keperawatan (FKep)',
+        'Kedokteran Gigi (FKG)',
+        'Ilmu Kelautan (FIK)',
+        'Psikologi (FPsi)',
+        'Ilmu Budaya (FIB)',
+      ],
+      default: 'Umum' // Default untuk Sekolah Pascasarjana dan umum
+    },
   },
-  { timestamps: true } // Tambahkan opsi timestamps
+  { timestamps: true }
 );
 
 export const PostModel = mongoose.model("Post", PostSchema);
+
+
 
 // Get all posts
 export const getPosts = async () => {
